b3633ee89f2a9e69596c77759197a3a0454acb61
Eric J. Duran <eric.duran7@gmail.com>
2017-02-20 21:46:52 -0500

    Server.js clean-up

--- src/server.js
+++ src/server.js
@@ -1,13 +1,5 @@
 const server = require('./core/server');
 
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-
-
-import App from './core/system/components/App';
-import Html from './core/system/components/Html';
-
-
 server.listen(3000, () => {
     console.log('App listening on port 3000!')
 });

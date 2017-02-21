95552928a2e138b5998aab43b21899527dd7baff
Eric J. Duran <eric.duran7@gmail.com>
2017-02-20 22:40:11 -0500

    Updating article component

--- src/plugins/content/component/Article.js
+++ src/plugins/content/component/Article.js
@@ -5,27 +5,25 @@ import {Link} from 'react-router';
 const formSchema = {
   title: "Article",
   type: "object",
-  required: ["name"],
   properties: {
     name: {type: "string", title: "Name"},
-  }
+    articleBody: {type: "string", title: "Body"}
+  },
+  required: [
+    "name"
+  ]
 };
 
 const formUI = {
   "name": {
     "ui:autofocus": true
   },
+  "articleBody": {
+    "ui:widget": "textarea"
+  }
 };
 
 export default class Article extends React.Component {
-  // EventEmitter for form? Damn, now we need an isomorphic eventEmmitter, yo this is a lot of work ;-p
-  // Also might make sense to wrap this in something, `extends Core.Component` or `Core.FormComponent` only if you
-  // want to have your form be alter-able via an Emitter. Can keep using React.Component if form or no emmiter needed.
-  // look into the Component life cycle for a refresher to see if there's anyting there we can take advantage and use.
-
-  // We can also load the view Components into "State" and then allow other plugins to replace the state "view" with their
-  // own view. So instead of adding to form, you would replace it with your own.
-
   render() {
     return (
       <div className="row justify-content-center">

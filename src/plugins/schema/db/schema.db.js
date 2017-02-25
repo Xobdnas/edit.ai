b78d6b59d483be81b37bad460144deff1433e128
Eric J. Duran <eric.duran7@gmail.com>
2017-02-25 10:04:36 -0500

    Adding Schema table for name, label, form_schema, ui_schema

---
+++ src/plugins/schema/db/schema.db.js
@@ -0,0 +1,20 @@
+// TODO: Want us to inclue via "core/" package aka "core/db"
+// What is below is just an idea, not real.
+// const db = require('core/db')
+import db from '../../../core/db';
+import Sequelize from 'sequelize';
+
+db.define('schemas', {
+  name: {
+    type: Sequelize.STRING,
+  },
+  label: {
+    type: Sequelize.STRING,
+  },
+  form_schema: {
+    type: Sequelize.JSON
+  },
+  ui_schema: {
+    type: Sequelize.JSON,
+  }
+});

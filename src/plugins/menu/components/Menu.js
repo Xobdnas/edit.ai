deb5241e43fa5d0f16bd93ad3edd4177c1cb54dd
Eric J. Duran <eric.duran7@gmail.com>
2017-03-01 23:28:36 -0500

    Fixing annoying radium issue

--- src/plugins/menu/components/Menu.js
+++ src/plugins/menu/components/Menu.js
@@ -8,7 +8,7 @@ export default class Menu extends React.Component {
 
   render() {
     return (
-      <ReactBurgerMenu noOverlay left width={ 200 } >
+      <ReactBurgerMenu noOverlay left width={ 200 } radiumConfig={{ userAgent: 'all' }}>
         <Link to="/login" className="nav-link" activeClassName="active" >Login</Link>
         <Link to="/reset" className="nav-link">Reset</Link>
         <Link to="/create" className="nav-link" activeClassName="active" >Article</Link>

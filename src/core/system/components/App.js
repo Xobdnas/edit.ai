8be4ac0188a1ceaf7e2cc5bb7211221a393660a5
Eric J. Duran <eric.duran7@gmail.com>
2017-03-05 00:37:02 -0500

    Cleaning header and menu

--- src/core/system/components/App.js
+++ src/core/system/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Menu from '../../../plugins/menu/components/Menu';
+import '../app.scss';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Menu />
+      <div className="">
+        <header>
+          <Menu />
+        </header>
         <div id="outer-container">
           <div id="page-wrap">
             {this.props.children}

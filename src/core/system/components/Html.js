9dae8da18341d9452b11ccff089f13134bfddc3f
Eric J. Duran <eric.duran7@gmail.com>
2017-03-05 02:03:46 -0500

    Adding font-awesome

--- src/core/system/components/Html.js
+++ src/core/system/components/Html.js
@@ -7,6 +7,7 @@ function Html({content}) {
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" />
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
       <link rel="stylesheet" href="/css/styles.css" />
     </head>
     <body>

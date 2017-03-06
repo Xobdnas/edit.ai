a5f9cd7627f1c78cd1e9eb6db39ac15e558f07aa
Eric J. Duran <eric.duran7@gmail.com>
2017-03-05 23:46:57 -0500

    Add user header to topbar and faker for fake data

--- src/core/system/components/App.js
+++ src/core/system/components/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import { Dropdown, Image } from 'semantic-ui-react';
 import Menu from '../../../plugins/menu/components/Menu';
 import '../app.scss';
 
 export default class App extends Component {
+  // TODO: Move this into "User settings,management component".
+  options = [
+    { key: 'user', text: 'Account', icon: 'user' },
+    { key: 'settings', text: 'Settings', icon: 'settings' },
+    { key: 'sign-out', text: 'Sign Out', icon: 'sign out' },
+  ];
+
+  trigger = (
+    <span>
+      <Image avatar src="https://avatars0.githubusercontent.com/u/118016?v=3&s=128" /> Eric J. Duran
+    </span>
+  );
+
   render() {
     return (
-      <div className="">
-        <header>
+      <div>
+        <header className="header">
           <Menu />
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col push-md-10">
+                <Dropdown trigger={this.trigger} options={this.options} pointing='top left' icon={null} />
+              </div>
+            </div>
+          </div>
         </header>
         <div id="outer-container">
           <div id="page-wrap">
